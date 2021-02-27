9fb9c193388a47d8dcfb5ecd6958f45382c7fe00
cukiermichael <michaelcukier@hotmail.com>
2021-02-27 14:46:44 +0000

    more heatmapping

---
+++ db_api/plots/dump.py
@@ -0,0 +1,17 @@
+
+
+
+def create_config_for_sql_query(nb_seated):
+    pass
+
+
+
+def get_sql_data(config, database_file_path: str, player_name) -> dict:
+    pass
+
+
+def transform_cards(cards_by_position: dict) -> dict:
+    # eg {'UTG': ['As Ad', 'Tc 9s'], ...}  --> {'UTG': ['AA', 'T9o'], ...}
+    pass
+
+def transform_list_of_hands_to_frequencies
